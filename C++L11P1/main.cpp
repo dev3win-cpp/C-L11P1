552bc07f44dbc9b80dd7e5b54506b02d32c9f6df
Tadeusz <tadeusz.potoniec@parhelium.com>
2023-01-24 12:25:34 +0100

    Initial Commit

---
+++ C++L11P1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++L11P1
+//
+//  Created by Tadeusz Potoniec on 24/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
